8040a3ba5e1178e6ab29c56861bf8f327684f59e
Akshat449 <akshatkr449@gmail.com>
2026-08-16 02:38:34 +0530

    Set up Express server, MongoDB connection, and User model

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI;
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: { type: String, default: "" },
+    workspaces: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workspace' }],
+}, 
+{ timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
